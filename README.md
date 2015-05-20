22bf212a577b3aa85aa414ad28b6ccf0b06fd62a
Dallas Koncir <me@dallaskoncir.com>
2015-05-20 17:16:49 -0400

    setup basic plugin and demo page

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-#fsVid - A simple jQuery plugin for full screen background videos
+#fsVid - A simple jQuery plugin for full screen background videos with automatically generated poster images
+
+A work in progress.
--- js/fsVid.js
+++ js/fsVid.js
@@ -1,3 +1,31 @@
-$.fn.fsVid = function() {
-  
+$.fn.fsVid = function(options) {
+
+  // Generate poster image using Popcorn.js
+  // Store generated poster image as a variable
+  var pop = Popcorn('video'), poster;
+
+  pop.capture({
+    at: 1
+  });
+
+  console.log(pop);
+
+  // Set defaults
+  var settings = $.extend({
+    autoplay: true,
+    loop: true,
+    muted: true,
+    poster: pop
+  }, options);
+
+  // Apply attributes to selected element
+  this.attr({
+    autoplay: settings.autoplay,
+    loop: settings.loop,
+    muted: settings.muted,
+    poster: pop.settings
+  })
+  .addClass('fsVid');
+
+  return this;
 };
