4b204e1d766ce8207c3de229aea2ef67ce69111f
Dallas Koncir <me@dallaskoncir.com>
2015-05-23 23:27:20 -0400

    added arrows and animated scroll

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(function() {
+  // Use fsVid plugin
   $('video').fsVid();
+
+  // Scroll down when arrows are clicked
+  $('.arrows a').on('click', function(e) {
+    e.preventDefault();
+    var winH = $(window).height() + 50;
+    $('body,html').animate({ scrollTop: winH }, 'fast');
+  });
 });
